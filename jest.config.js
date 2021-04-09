29ca7f334dbe94e99e5a7c334467c92b3e358f2b
rpidanny <abhishekmaharjan1993@gmail.com>
2021-04-09 16:50:43 +0200

    feat: add semantic release and release workflow

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,23 @@
+const baseConfig = require('./jest-base')
+
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
+  ...baseConfig,
+  rootDir: '.',
+  roots: ['<rootDir>/__tests__'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['<rootDir>/src/__mocks__/*'],
+  setupFilesAfterEnv: ['./__tests__/setup-test.js'],
+  cacheDirectory: '<rootDir>/.cache/unit',
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!**/node_modules/**', '!**/vendor/**'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: [['lcov', { projectRoot: './' }], 'text'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
   },
-  verbose: true
 }
